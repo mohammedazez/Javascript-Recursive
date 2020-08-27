b66d4ebb452acf4c418ab87dc3846ed311da31ba
azezmohammed <muhamadaziz047@gmail.com>
2020-08-27 16:09:49 +0700

    Javascript Recursive

---
+++ recursive.js
@@ -0,0 +1,17 @@
+// 1. Buatlah rekursive function untuk menghitung angka faktorial
+// Contoh perhitungan angka faktorial:
+// 4! = 4 * 3!
+// 4! = 4 * 3 * 2!
+// 4! = 4 * 3 * 2 * 1
+// 4! = 2
+
+// 2. Buatlah rekursive function yang dapat menampilkan deret angka genap sampai N Input.
+// Contoh : Jika diberi argumen angka 5, maka hasilnya adalah 0 2 4
+
+// 3. Buatlah rekursive function yang dapat menghasilkan angka seperti Ini
+// function(1) = 3
+// function(2) = 6
+// function(3) = 12
+// function(4) = 24
+// function(5) = 48
+// function(n) = ....
