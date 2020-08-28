4687683f0afa78ba2779ddd7f318c1e5bc01c574
azezmohammed <muhamadaziz047@gmail.com>
2020-08-28 08:22:00 +0700

    Javascript Recursive

--- recursive.js
+++ recursive.js
@@ -14,6 +14,20 @@ console.log(faktorial(4));
 // 2. Buatlah rekursive function yang dapat menampilkan deret angka genap sampai N Input.
 // Contoh : Jika diberi argumen angka 5, maka hasilnya adalah 0 2 4
 
+function deretAngkaGenap(nilai) {
+  if (nilai == 1) {
+    console.log(0);
+  } else if (nilai % 2 == 0) {
+    deretAngkaGenap(nilai - 2);
+    console.log(nilai);
+  } else {
+    deretAngkaGenap(nilai - 2);
+    console.log(nilai - 1);
+  }
+}
+
+deretAngkaGenap(5);
+
 // 3. Buatlah rekursive function yang dapat menghasilkan angka seperti Ini
 // function(1) = 3
 // function(2) = 6
